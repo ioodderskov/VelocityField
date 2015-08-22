2938f40f8a9ee9e617e021bf0059b1be2efc3990
Io Odderskov <io.odderskov@gmail.com>
2015-08-22 16:23:26 +0200

    committing again before converting to Hg.

--- analysis/haloplots/generate_plots.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# HALOPLOTS
-exe_haloplots=generate_haloplots.py
-
-python $exe_haloplots
-
---
+++ code/ios_analysishelpers.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import division
+import scipy as sp
+import matplotlib.pyplot as mplt
+from gplot import Plot
+#plt = Plot('io_latex_full','full')
+#import sys
+#sys.path.insert(0, '/home/io/Dropbox/Projekter/Hubble/VelocityField/code'   )
+import hubble_classes as hc
+import hubble_functions as hf
+import powerspectrum_functions as ps
+import astropysics.constants as ac # link: https://pythonhosted.org/Astropysics/coremods/constants.html
+# You can do, for example: ac.WMAP7Cosmology.omegaC
+
+print "Io, her kan du lægge alle dine små hjælpefunktioner."
